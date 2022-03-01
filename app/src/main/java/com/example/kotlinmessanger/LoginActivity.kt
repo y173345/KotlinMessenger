22ad15ef6cec446d6c43ea4536e354ab487a0c15
Yuta Nakayama <nakayama@aisl.cs.tut.ac.jp>
2022-03-01 21:59:21 +0900

    add: view of signin

--- app/src/main/java/com/example/kotlinmessanger/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmessanger/LoginActivity.kt
@@ -1,4 +1,13 @@
 package com.example.kotlinmessanger
 
-class LoginActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlinmessenger.R
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
 }
