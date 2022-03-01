08a4b51fb42d5e8b1190ff06ca27f314f5a978be
Yuta Nakayama <nakayama@aisl.cs.tut.ac.jp>
2022-03-01 21:57:19 +0900

    add: view of sign up

---
+++ app/src/main/java/com/example/kotlinmessanger/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinmessanger
+
+class LoginActivity {
+}
--- app/src/main/java/com/example/kotlinmessanger/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessanger/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.kotlinmessenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.kotlinmessanger.LoginActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = email_edittext_register.text.toString()
-        val password = password_edittext_register.text.toString()
+        register_button_register.setOnClickListener {
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
 
-        Log.d("MainActivity", "Email is: " + email)
-        Log.d("MainActivity", "password: $password")
+            Log.d("MainActivity", "Email is: " + email)
+            Log.d("MainActivity", "password: $password")
 
+            already_have_account_text_view.setOnClickListener {
+                Log.d("MainActivity", "Try to show login activity")
 
+                // launch the login activity somehow
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+
+            }
+        }
     }
 }
